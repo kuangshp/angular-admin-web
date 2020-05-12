ba37658248fbd758d95baca682b56efec32519f7
kuangshp <kuangshp@126.com>
2020-05-12 10:23:35 +0800

    修改登录页面

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,7 +1,8 @@
-export const baseName = 'order-admin';
+export const baseName = 'ncre-web';
 export const X_USER_TOKEN = 'X-User-Token';
 export const userInfo = 'userInfo';
 export const X_ORG_ID = 'x-org-id';
 export const X_ORIGIN = 'x-origin';
 export const X_USER_ID = 'x-user-id';
 export const X_OPERATED_PRODUCT = 'X-Operated-Product';
+export const authToken = 'token';
