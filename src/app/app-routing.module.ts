de91d0d88513aaae55e1da9849aac3bd74acc0ff
kuangshp <kuangshp@126.com>
2020-05-12 10:19:30 +0800

    页面展示

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+import { LayoutComponent } from './views/layout/layout.component';
+import { LoginComponent } from './views/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivate: [AuthGuard], // 守卫
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        data: {
+          title: 'home',
+        },
+        canActivateChild: [AuthGuard],
+        loadChildren: () => import('./views/home/home.module').then(res => res.HomeModule)
+      },
+      {
+        path: 'setting',
+        canActivateChild: [AuthGuard],
+        loadChildren: () => import('./views/setting/setting.module').then(res => res.SettingModule)
+      },
+
+    ]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   baseUrl: '',
-  ignoreToken: [],
+  ignoreToken: ['login', 'logout'],
   parentId: 'ncre-web',
 };
