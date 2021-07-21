5d0c1e577875d4ba9c985dfe4cf3473dda695ef2
kuangshp <kuangshp@126.com>
2021-07-21 11:50:40 +0800

    feat: 按钮组件

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,8 +5,9 @@ import { LayoutModule } from './layout/layout.module';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from '../module/shared/shared.module';
 import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 @NgModule({
   declarations: [LoginComponent],
-  imports: [SharedModule, PagesRoutingModule, LayoutModule, NzInputModule],
+  imports: [SharedModule, PagesRoutingModule, LayoutModule, NzInputModule, NzButtonModule],
 })
 export class PagesModule {}
