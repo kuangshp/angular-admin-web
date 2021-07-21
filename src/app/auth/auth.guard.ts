5de2908e8961fdfae258e42fb8dbf50f23384e97
kuangshp <kuangshp@126.com>
2021-07-21 11:23:15 +0800

    feat: 未登录到登录页面

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -3,27 +3,65 @@ import {
   ActivatedRouteSnapshot,
   CanActivate,
   CanActivateChild,
+  Router,
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { X_USER_TOKEN } from '../constants';
+import { storage } from '../utils';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate, CanActivateChild {
+  constructor(private router: Router) {}
+  /**
+   * @Author: 水痕
+   * @Date: 2021-07-21 11:16:47
+   * @LastEditors: 水痕
+   * @Description: 导航需要进入的页面
+   * @param {*}
+   * @return {*}
+   */
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     console.log(route, state);
-    return true;
+    return this.checkLogin();
   }
+
+  /**
+   * @Author: 水痕
+   * @Date: 2021-07-21 11:17:23
+   * @LastEditors: 水痕
+   * @Description: 导航需要进入的子路由
+   * @param {*}
+   * @return {*}
+   */
   canActivateChild(
     childRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     console.log(childRoute, state);
-    return true;
+    return this.checkLogin();
+  }
+
+  /**
+   * @Author: 水痕
+   * @Date: 2021-07-21 11:20:46
+   * @LastEditors: 水痕
+   * @Description: 判断当前用户是否登录
+   * @param {*}
+   * @return {*}
+   */
+  private checkLogin(): Observable<boolean> | boolean {
+    if (storage.getItem(X_USER_TOKEN)) {
+      return of(true);
+    } else {
+      this.router.navigateByUrl('/login');
+      return false;
+    }
   }
 }
---
+++ src/app/constants/config.ts
@@ -0,0 +1,8 @@
+export const baseName = 'admin-web';
+export const X_USER_TOKEN = 'X-User-Token';
+export const userInfo = 'userInfo';
+export const X_ORG_ID = 'x-org-id';
+export const X_ORIGIN = 'x-origin';
+export const X_USER_ID = 'x-user-id';
+export const X_OPERATED_PRODUCT = 'X-Operated-Product';
+export const HTTP_TIMEOUT = 1000 * 30; // http请求30秒超时
