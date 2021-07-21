c29e34efc2c60f5ac264ebad05587bfe399856dd
kuangshp <kuangshp@126.com>
2021-07-21 09:09:52 +0800

    feat: 配置antd组件库

---
+++ README.md
@@ -0,0 +1,19 @@
+## 基础的使用
+
+- 1、下载代码
+
+  ```shell
+  git clone git@github.com:kuangshp/ng9-ng-zorro-admin.git
+  ```
+
+- 2、安装依赖包
+
+  ```shell
+  npm install
+  ```
+
+- 3、运行命令
+
+  ```shell
+  npm run start
+  ```
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  {
+    path: 'welcome',
+    loadChildren: () => import('./pages/welcome/welcome.module').then((m) => m.WelcomeModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
