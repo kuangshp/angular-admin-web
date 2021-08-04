67c4aac832ee14903da5a40d07cd0ad7bc6b301b
kuangshp <kuangshp@126.com>
2021-08-04 22:02:49 +0800

    fix: 修复路由的问题

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LayoutComponent } from './pages/layout/layout.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: LayoutComponent,
     canActivate: [AuthGuard],
     children: [
@@ -34,11 +34,6 @@ const routes: Routes = [
         canActivateChild: [AuthGuard],
         loadChildren: () => import('./pages/setting/setting.module').then((m) => m.SettingModule),
       },
-      {
-        path: '**',
-        pathMatch: 'full',
-        redirectTo: '/home',
-      },
     ],
   },
   {
@@ -46,9 +41,9 @@ const routes: Routes = [
     loadChildren: () => import('./pages/login/login.module').then((m) => m.LoginModule),
   },
   {
-    path: '',
+    path: '**',
     pathMatch: 'full',
-    redirectTo: '/login',
+    redirectTo: '/home',
   },
 ];
 
